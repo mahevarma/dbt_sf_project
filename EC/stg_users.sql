4f3c8550c0efb9c1410e610bc5010706aa735f18
shrutikam0910@gmail.com <shrutikam0910@gmail.com>
2024-08-23 15:11:17 +0000

    commit

--- EC/stg_users.sql
+++ EC/stg_users.sql
@@ -1,13 +1,27 @@
-{{config(materialized='table')}}
+{{ config(materialized="table") }}
 
+with
+    user_data as (
+        select
+            id as user_id,
+            first_name,
+            last_name,
+            email,
+            age,
+            gender,
+            state,
+            street_address,
+            postal_code,
+            city,
+            country,
+            latitude,
+            longitude,
+            traffic_source,
+            cast(substring('created_at', 1, 18) as datetime)
 
-select
-(
-id as user_id, first_name,	last_name, email, age, gender, 
-state, street_address, postal_code,
-city, country, latitude	longitude, traffic_source	
-cast(substring('created_at',1,18) as datetime)
+        from {{ source('src_ec','users')}}
 
-from {{ source('src_ec','users')}}
+    )
 
-)
+select *
+from user_data

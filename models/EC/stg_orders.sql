702e945ee5f6dcf53c42885f70c85b2f7643a3c0
Shrutika Murudkar <shrutikam0910@gmail.com>
2024-09-01 14:32:18 +0000

    commit the code

--- models/EC/stg_orders.sql
+++ models/EC/stg_orders.sql
@@ -9,10 +9,10 @@
             gender,
             num_of_item, 
 
-            cast(substring('ordered_at', 1, 18) as datetime) as ordered_at,
-            cast(substring('returned_at', 1, 18) as datetime) as returned_at,
-            cast(substring('shipped_at', 1, 18) as datetime) as shipped_at,
-            cast(substring('delivered_at', 1, 18) as datetime) as delivered_at
+            
+            cast(substring(returned_at, 1, 18) as datetime) as returned_at,
+            cast(substring(shipped_at, 1, 18) as datetime) as shipped_at,
+            cast(substring(delivered_at, 1, 18) as datetime) as delivered_at
 
         from {{ source('src_ec','orders')}}
 

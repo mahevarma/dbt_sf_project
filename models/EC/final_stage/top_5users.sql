4737005db38bbde3fe45147d3af07602bab65803
Shrutika Murudkar <shrutikam0910@gmail.com>
2024-09-04 12:04:32 +0000

    commit

--- models/EC/final_stage/top_5users.sql
+++ models/EC/final_stage/top_5users.sql
@@ -18,26 +18,27 @@ temp_orders as(
 UserSpending as(
 
     SELECT
-        first_name,
-        last_name,
+        temp_order_items.user_id,
         SUM(sale_price) AS TotalAmountSpent
         from 
+
         temp_user join temp_order_items
         on 
         temp_user.user_id = temp_order_items.user_id 
         join 
         temp_orders
         on temp_orders.order_id = temp_order_items.order_id
+        
         WHERE
         temp_orders.status NOT IN ('Cancelled', 'Returned')
     GROUP BY
-        first_name, last_name
+       temp_order_items.user_id
+      
 )
 
 
 SELECT
-    first_name,
-    last_name,
+    user_id,
     TotalAmountSpent
 FROM
     UserSpending
---
+++ models/EC/final_stage/top_users.sql
@@ -0,0 +1,49 @@
+{{ config(materialized="table") }}
+
+with temp_user as(
+
+    select * from {{ref("stg_users")}}
+),
+
+temp_order_items as(
+
+    select * from {{ref("stg_order_items")}}
+),
+
+temp_orders as(
+
+    select * from {{ref("stg_orders")}}
+),
+
+UserSpending as(
+
+    SELECT
+        first_name,
+        last_name,
+        SUM(sale_price) AS TotalAmountSpent
+        from 
+
+        temp_user join temp_order_items
+        on 
+        temp_user.user_id = temp_order_items.user_id 
+        join 
+        temp_orders
+        on temp_orders.order_id = temp_order_items.order_id
+        
+        WHERE
+        temp_orders.status NOT IN ('Cancelled', 'Returned')
+    GROUP BY
+       first_name, last_name
+      
+)
+
+
+SELECT
+    first_name,
+    last_name,
+    TotalAmountSpent
+FROM
+    UserSpending
+ORDER BY
+    TotalAmountSpent DESC
+LIMIT 5
